3f697bb4b82169cd6effe4924104b39749ea9e8b
jvmistica <zombossjoey@tutanota.com>
2024-03-24 13:49:28 +0100

    closes #4: create basic item

---
+++ item.py
@@ -0,0 +1,24 @@
+class Item:
+    def __init__(self, name, count, price):
+        self.name = name
+	self.count = count
+	self.price = price
+
+    def details(self):
+        return f'Name: {self.name}\nCount: {self.count}\nPrice: {self.price}'
+
+    def sell(self, count):
+        self.count -= count
+        return f'{self.name}\'s count has decreased to {self.count}'
+
+    def restock(self, count):
+        self.count += count
+        return f'{self.name}\'s count has increased to {self.count}'
+
+    def update_price(self, new_price):
+        self.price = new_price
+        return f'{self.name}\'s price is now {self.price}'
+
+    def remove(self):
+        self.count = 0
+        return f'{self.name} has been removed from the inventory'
