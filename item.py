f687c37efdb4cfca6792655354c16dbda12c49d9
jvmistica <zombossjoey@tutanota.com>
2024-03-30 17:10:07 +0100

    docs: add comments

--- item.py
+++ item.py
@@ -6,20 +6,40 @@ class Item:
         self.counter = counter
 
     def details(self):
+        """
+        Returns the details of the item.
+        """
+
         return f'Name: {self.name}\nCount: {self.count}\nPrice: {self.price}'
 
     def sell(self, count):
+        """
+        Sells an item.
+        """
+
         self.count -= count
         return f'{self.name}\'s count has decreased to {self.count}'
 
     def restock(self, count):
+        """
+        Re-stocks an item.
+        """
+
         self.count += count
         return f'{self.name}\'s count has increased to {self.count}'
 
     def update_price(self, new_price):
+        """
+        Updates the price of an item.
+        """
+
         self.price = new_price
         return f'{self.name}\'s price is now {self.price}'
 
     def remove(self):
+        """
+        Removes an item.
+        """
+
         self.count = 0
         return f'{self.name} has been removed from the inventory'
