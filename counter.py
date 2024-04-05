cfbb682c8ad3392b1173d9cd2239cd2c19322118
jvmistica <zombossjoey@tutanota.com>
2024-04-05 23:06:48 +0200

    feat: issue #11 return error messages [counter]

--- counter.py
+++ counter.py
@@ -1,4 +1,3 @@
-import json
 import uuid
 from employee import Employee
 from item import Item
@@ -20,22 +19,30 @@ class Counter:
         """
 
         item_list = [{'id': i.id, 'name': i.name, 'count': i.count, 'price': i.price, 'counter': i.counter} for i in self.items if len(self.items) > 0]
-        details = {
+        return {
             'id': self.id,
             'name': self.name,
             'status': self.status,
             'slots': self.slots,
             'items': item_list
         }
-        return json.dumps(details)
 
     def remove(self) -> dict:
         """
         Removes a counter.
         """
 
+        if self.status == 'inactive':
+            return {
+                'error': True,
+                'error_message': 'Counter is already inactive'
+            }
+
         self.status = 'inactive'
-        return f'{self.name} has been removed from the display'
+        return {
+            'error': False,
+            'error_message': None
+        }
 
     def assign_employee(self, employee: Employee) -> dict:
         """
@@ -43,11 +50,10 @@ class Counter:
         """
 
         self.employee = employee
-        if employee == '':
-            return f'Counter {self.name} has no employee assigned'
-        
-        return f'{employee.name} has been assigned to counter {self.name}'
-
+        return {
+            'error': False,
+            'error_message': None
+        }
 
     def add_item(self, item: Item) -> dict:
         """
@@ -55,14 +61,23 @@ class Counter:
         """
 
         if not item.counter:
-            return f'{item.name} is not a type of item that can be added to a counter'
+            return {
+                'error': True,
+                'error_message': f'{item.name} is not a type of item that can be added to a counter'
+            }
 
         if self.slots == 0:
-            return f'Item cannot be added to the counter, all slots are taken'
+            return {
+                'error': True,
+                'error_message': f'Item cannot be added to the counter, all slots are taken'
+            }
 
         self.items.append(item)
         self.slots -= 1
-        return f'Item has been added to the counter'
+        return {
+            'error': False,
+            'error_message': None
+        }
 
     def replace_item(self, current_item: Item, new_item: Item) -> dict:
         """
@@ -70,20 +85,39 @@ class Counter:
         """
 
         if not new_item.counter:
-            return f'{new_item.name} is not a type of item that can be added to a counter'
+            return {
+                'error': True,
+                'error_message': f'{new_item.name} is not a type of item that can be added to a counter'
+            }
 
         for n, item in enumerate(self.items):
             if item == current_item:
                 self.items[n] = new_item
-                return f'Discarded: {current_item.name}\nAdded: {new_item.name}'
-        raise ValueError(f'Item \'{current_item.name}\' does not exist')
+                return {
+                    'error': False,
+                    'error_message': None
+                }
+                # return f'Discarded: {current_item.name}\nAdded: {new_item.name}'
+        return {
+            'error': True,
+            'error_message': f'Item \'{current_item.name}\' does not exist'
+        }
 
     def remove_item(self, item: Item) -> dict:
         """
         Removes an item from a counter.
         """
 
-        # TODO: add check if removal is successful
-        self.items.remove(item)
+        try:
+            self.items.remove(item)
+        except ValueError:
+            return {
+                'error': True,
+                'error_message': f'Item \'{item.name}\' does not exist'
+            }
+
         self.slots += 1
-        return f'Item "{item.name}" has been removed from the counter'
+        return {
+            'error': False,
+            'error_message': None
+        }
--- test_counter.py
+++ test_counter.py
@@ -1,13 +1,9 @@
-import json
 import unittest
 from counter import Counter
 from item import Item
 from employee import Employee
 from helper import create_items
 
-# TODO:
-# setup and teardown methods
-# break down and clean-up tests
 class TestCounter(unittest.TestCase):
 
     def test_new_counter(self):
@@ -17,16 +13,17 @@ class TestCounter(unittest.TestCase):
 
     def test_details(self):
         counter = Counter('Counter #1')
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 8)
         self.assertEqual(len(result['items']), 0)
 
         item = Item('chocolate kariman', 20, 1.99, True)
         add_result = counter.add_item(item)
-        self.assertEqual(add_result, 'Item has been added to the counter')
+        self.assertFalse(add_result['error'])
+        self.assertIsNone(add_result['error_message'])
 
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 7)
         self.assertEqual(len(result['items']), 1)
@@ -37,28 +34,39 @@ class TestCounter(unittest.TestCase):
 
     def test_remove(self):
         counter = Counter('Counter #1')
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 8)
         self.assertEqual(result['status'], 'active')
         self.assertEqual(len(result['items']), 0)
 
+        # valid removal
         remove_result = counter.remove()
-        self.assertEqual(remove_result, 'Counter #1 has been removed from the display')
+        self.assertFalse(remove_result['error'])
+        self.assertIsNone(remove_result['error_message'])
+        self.assertEqual(counter.status, 'inactive')
+
+        # invalid removal
+        remove_result = counter.remove()
+        self.assertTrue(remove_result['error'])
+        self.assertEqual(remove_result['error_message'], 'Counter is already inactive')
         self.assertEqual(counter.status, 'inactive')
 
     def test_assign_employee(self):
-        # assign employee
         counter = Counter('Counter #1')
         employee = Employee('Employee 1', 70000, 80, 100)
-        result = counter.assign_employee(employee)
-        self.assertEqual(counter.employee, employee)
-        self.assertEqual(result, 'Employee 1 has been assigned to counter Counter #1')
 
-        # unassign employee
-        result = counter.assign_employee('')
-        self.assertEqual(counter.employee, '')
-        self.assertEqual(result, 'Counter Counter #1 has no employee assigned')
+        # assign employee
+        result = counter.assign_employee(employee)
+        self.assertFalse(result['error'])
+        self.assertIsNone(result['error_message'])
+        self.assertEqual(counter.employee, employee)
+
+        # # unassign employee
+        # result = counter.assign_employee('')
+        # self.assertFalse(result['error'])
+        # self.assertIsNone(result['error_message'])
+        # self.assertEqual(counter.employee, '')
 
     def test_add_item(self):
         counter = Counter('Counter #1')
@@ -67,24 +75,28 @@ class TestCounter(unittest.TestCase):
         for i, item in enumerate(items):
             result = counter.add_item(item)
             if i < 8:
-                # valid
-                self.assertEqual(result, 'Item has been added to the counter')
+                # valid addition
+                self.assertFalse(result['error'])
+                self.assertIsNone(result['error_message'])
             else:
-                # no slots left
-                self.assertEqual(result, 'Item cannot be added to the counter, all slots are taken')
+                # invalid addition, no slots left
+                self.assertTrue(result['error'])
+                self.assertEqual(result['error_message'], 'Item cannot be added to the counter, all slots are taken')
 
-        # not a counter item
+        # invalid addition, not a counter item
         invalid_item = Item('Item 10', 30, 1.99, False)
         result = counter.add_item(invalid_item)
-        self.assertEqual(result, 'Item 10 is not a type of item that can be added to a counter')
+        self.assertTrue(result['error'])
+        self.assertEqual(result['error_message'], 'Item 10 is not a type of item that can be added to a counter')
 
     def test_replace_item(self):
         counter = Counter('Counter #1')
         item = Item('chocolate kariman', 20, 1.99, True)
         add_result = counter.add_item(item)
-        self.assertEqual(add_result, 'Item has been added to the counter')
+        self.assertFalse(add_result['error'])
+        self.assertIsNone(add_result['error_message'])
         
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 7)
         self.assertEqual(len(result['items']), 1)
@@ -93,20 +105,22 @@ class TestCounter(unittest.TestCase):
         # valid replace
         new_item = Item('dumplings', 25, 1.59, True)
         replace_result = counter.replace_item(item, new_item)
-        self.assertEqual(replace_result, f'Discarded: {item.name}\nAdded: {new_item.name}')
+        self.assertFalse(replace_result['error'])
+        self.assertIsNone(replace_result['error_message'])
 
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 7)
         self.assertEqual(len(result['items']), 1)
         self.assertEqual(result['items'][0]['name'], 'dumplings')
 
-        # invalid replace: item being replaced does not exist
+        # invalid replace, item being replaced does not exist
         new_item = Item('siopao', 25, 1.59, True)
-        with self.assertRaises(ValueError):
-            counter.replace_item(item, new_item)
+        replace_result = counter.replace_item(item, new_item)
+        self.assertTrue(replace_result['error'])
+        self.assertEqual(replace_result['error_message'], 'Item \'chocolate kariman\' does not exist')
 
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 7)
         self.assertEqual(len(result['items']), 1)
@@ -116,18 +130,31 @@ class TestCounter(unittest.TestCase):
         counter = Counter('Counter #1')
         item = Item('chocolate kariman', 20, 1.99, True)
         add_result = counter.add_item(item)
-        self.assertEqual(add_result, 'Item has been added to the counter')
+        self.assertFalse(add_result['error'])
+        self.assertIsNone(add_result['error_message'])
         
-        result = json.loads(counter.details())
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 7)
         self.assertEqual(len(result['items']), 1)
         self.assertEqual(result['items'][0]['name'], 'chocolate kariman')
 
+        # valid removal
         remove_result = counter.remove_item(item)
-        self.assertEqual(remove_result, 'Item "chocolate kariman" has been removed from the counter')
+        self.assertFalse(remove_result['error'])
+        self.assertIsNone(remove_result['error_message'])
         
-        result = json.loads(counter.details())
+        result = counter.details()
+        self.assertEqual(result['name'], 'Counter #1')
+        self.assertEqual(result['slots'], 8)
+        self.assertEqual(len(result['items']), 0)
+
+        # invalid removal
+        remove_result = counter.remove_item(item)
+        self.assertTrue(remove_result['error'])
+        self.assertEqual(remove_result['error_message'], 'Item \'chocolate kariman\' does not exist')
+        
+        result = counter.details()
         self.assertEqual(result['name'], 'Counter #1')
         self.assertEqual(result['slots'], 8)
         self.assertEqual(len(result['items']), 0)
